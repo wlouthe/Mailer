3c9a5783e4c9bd4d2957575856dbe58bb17de695
William Louthe <wlouthe@gmail.com>
2015-09-20 03:45:40 -0400

    Testing mode

--- mailer.php
+++ mailer.php
@@ -22,16 +22,5 @@ if(isset($_POST["username"]) && !empty($_POST["username"]) && isset($_POST["pass
 <div>Recipients (Please separate entries by commas):<br><textarea id="recipients"></textarea></div>
 
 </form>
-<?php
-$subject = '';
-$message = "";
-$headers = 'From: team@loganthebogan.com';
-$list = array("");
-
-foreach($list as $to)
-{
-	mail($to,$subject,$message,$headers);
-}
-?>
 </body>
 </html>
--- submitmailer.php
+++ submitmailer.php
@@ -4,6 +4,17 @@
 			echo $_POST["subject"];
 			echo $_POST["message"];
 			echo $_POST["recipients"];
+			/*
+			$subject = '';
+			$message = "";
+			$headers = 'From: team@loganthebogan.com';
+			$list = array("");
+
+			foreach($list as $to)
+			{
+				mail($to,$subject,$message,$headers);
+			}
+			//*/
 		?>
 		<a href="./mailer.php">Send Another?</a>
 	</body>
